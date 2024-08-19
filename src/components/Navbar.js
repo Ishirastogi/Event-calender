cd90d1816e8f2c79da6382b8a8506f7d216656c7
ishi rastogi <137077186+Ishirastogi@users.noreply.github.com>
2024-08-19 10:44:28 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-xl font-bold">
+        <Link to="/" className={`text-white text-xl ${styles.link}`}>
           Grey Scientific Labs
         </Link>
-        <Link to="/add-event" className="text-gray-300 hover:text-white">
-          Add Event
-        </Link>
+        <div className="flex space-x-4">
+          <Link to="/events" className="text-gray-300 hover:text-white">
+            All Events
+          </Link>
+          <Link to="/add-event" className="text-gray-300 hover:text-white">
+            Add Event
+          </Link>
+        </div>
       </div>
     </nav>
   );
